81e1fcef4c91c3a48097a802a4609a4a5f67b6eb
Nathan West <nathanwest@google.com>
2018-07-13 13:34:47 -0700

    Now considers the asignee

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import asyncio
 import json
 import collections
 
+WHOAMI_QUERY = '''
+query {
+  viewer {
+    id
+  }
+}
+'''
+
 ISSUES_QUERY = '''
 query($owner:String! $repo:String! $cursor:String) {
   repository(owner:$owner name:$repo) {
@@ -12,6 +20,11 @@ query($owner:String! $repo:String! $cursor:String) {
         title
         url
         number
+        assignees(first:100) {
+          nodes {
+            id
+          }
+        }
         labels(first:100) {
           nodes {
             name
@@ -36,51 +49,55 @@ query($owner:String! $repo:String! $cursor:String) {
 class Issue(collections.namedtuple("Issue", "title number url")):
 	pass
 
-
-async def get_issues(owner, repository, token, ignore_labels):
-	headers = {
-		"Authorization": "bearer {token}".format(token=token),
-		"Accept": "application/json",
-		"Content-Type": "application/json",
+async def get_my_id(*, session):
+	full_query = {
+		"query": WHOAMI_QUERY
 	}
 
+	async with session.post("https://api.github.com/graphql", json=full_query) as response:
+		return (await response.json())["data"]["viewer"]["id"]
+
+
+async def get_issues(*, session, owner, repository, ignore_labels, my_id):
 	ignore_labels = {label.lower() for label in ignore_labels}
 
-	async with aiohttp.ClientSession(headers=headers) as session:
-
-		async def get_page(cursor):
-			full_query = {
-				"query": ISSUES_QUERY,
-				"variables": {
-					"owner": owner,
-					"repo": repository,
-					"cursor": cursor
-				}
+	async def get_page(cursor):
+		full_query = {
+			"query": ISSUES_QUERY,
+			"variables": {
+				"owner": owner,
+				"repo": repository,
+				"cursor": cursor
 			}
-			async with session.post("https://api.github.com/graphql", json=full_query) as response:
-				data = await response.json()
+		}
+		async with session.post("https://api.github.com/graphql", json=full_query) as response:
+			data = await response.json()
 
-			issues_data = data["data"]["repository"]["issues"]
-			issues = issues_data["nodes"]
-			pageInfo = issues_data["pageInfo"]
-			cursor = pageInfo['endCursor'] if pageInfo['hasNextPage'] else None
+		issues_data = data["data"]["repository"]["issues"]
+		issues = issues_data["nodes"]
+		pageInfo = issues_data["pageInfo"]
+		cursor = pageInfo['endCursor'] if pageInfo['hasNextPage'] else None
 
-			return (cursor, issues)
+		return (cursor, issues)
 
-		request = get_page(None)
+	request = get_page(None)
 
-		while request is not None:
-			(cursor, issues) = await request
-			if cursor:
-				request = asyncio.ensure_future(get_page(cursor))
+	while request is not None:
+		(cursor, issues) = await request
 
-				# Allow the next request to initiate, so i/o is happening in the
-				# background
-				await asyncio.sleep(0)
-			else:
-				request = None
+		if cursor:
+			request = asyncio.ensure_future(get_page(cursor))
 
-			for issue in issues:
+			# Allow the next request to initiate, so i/o is happening in the
+			# background
+			await asyncio.sleep(0)
+		else:
+			request = None
+
+		for issue in issues:
+			# Always show it if I'm assigned
+			assignees = (assignee["id"] for assignee in issue["assignees"]["nodes"])
+			if my_id not in assignees:
 				# Check ignore_labels, commentor status
 				if any(
 					label["name"].lower() in ignore_labels
@@ -92,11 +109,11 @@ async def get_issues(owner, repository, token, ignore_labels):
 				if comments and comments[0]["authorAssociation"] in ("MEMBER", "OWNER", "COLLABORATOR"):
 					continue
 
-				yield Issue(
-					title=issue["title"].strip(),
-					url=issue["url"],
-					number=issue["number"],
-				)
+			yield Issue(
+				title=issue["title"].strip(),
+				url=issue["url"],
+				number=issue["number"],
+			)
 
 
 @autocommand(__name__, loop=True)
@@ -104,10 +121,25 @@ async def main(
 	owner,
 	repository,
 	token=None,
-	ignore_labels=""
+	ignore_labels="snooze"
 ):
-	async for issue in get_issues(owner, repository, token, ignore_labels.split()):
-		print("Issue {number}\t{title}".format(
-			number=issue.number,
-			title=issue.title,
-		))
+	headers = {
+		"Authorization": "bearer {token}".format(token=token),
+		"Accept": "application/json",
+		"Content-Type": "application/json",
+	}
+
+	async with aiohttp.ClientSession(headers=headers) as session:
+		my_id = await get_my_id(session=session)
+
+		async for issue in get_issues(
+			session=session,
+			owner=owner,
+			repository=repository,
+			ignore_labels=ignore_labels.split(),
+			my_id=my_id,
+		):
+			print("Issue {number}\t{title}".format(
+				number=issue.number,
+				title=issue.title,
+			))
